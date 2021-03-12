b2df20fc61c01646afe6b7090b1e72241954783c
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-12 16:20:06 -0400

    more progress on indexing

--- src/rdi-index.c
+++ src/rdi-index.c
@@ -4,9 +4,46 @@
 
 #include "read-rdi-common.h"
 
+int rdi_search_for_header_start(read_rdi_data_t* data) {
+    size_t buf_size;
+    unsigned char buf[1024];
+
+    while ((buf_size = fread(buf, sizeof(unsigned char), 1024 - 1, data->handle)) > 0) {
+        // search for the first byte (0x7f)
+        for (size_t i = 0; i < buf_size; i++) {
+            // if we have a match on the last character, we need to use the extra
+            // buffer slot to read in one character
+            if (buf[i] == 0x7f && (i == (buf_size - 1))) {
+                if (fread(&(buf[1024 - 1]), 1, 1, data->handle) != 1) {
+                    return 0;
+                } else if (buf[1024 - 1] == 0x7f) {
+                    fseek(data->handle, -2, SEEK_CUR);
+                    return 1;
+                }
+            } else if ((buf[i] == 0x7f) && (buf[i + 1] == 0x7f)) {
+                fseek(data->handle, i - buf_size, SEEK_CUR);
+            }
+        }
+    }
+
+    return 0;
+}
+
 SEXP readrdi_read_rdi_index_impl(void* data_void) {
     read_rdi_data_t* data = (read_rdi_data_t*) data_void;
     rdi_header_t header;
+
+    if (fseek(data->handle, data->offset, SEEK_SET) != 0) {
+        Rf_error("Can't seek to start offset %d", data->offset);
+    }
+
+    while (rdi_search_for_header_start(data)) {
+        if (fread(&header, sizeof(rdi_header_t), 1, data->handle) != 1) {
+            break;
+        }
+
+        
+    }
 
     // seek to initial data->offset
 
